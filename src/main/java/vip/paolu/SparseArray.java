9f73a961c0d6655d2e5d1e3f0aa86fc432e353b9
panjiangfeng <1415161920@qq.com>
2023-02-15 16:00:25 +0800

    feat:稀疏数组

---
+++ src/main/java/vip/paolu/SparseArray.java
@@ -0,0 +1,87 @@
+package vip.paolu;
+
+/**
+ * Description: 稀疏数组
+ * User: Pan
+ * Date: 2023-02-15-15:19
+ */
+public class SparseArray {
+    private int[][] array;
+    private int[][] sparseArray;
+    private int row = 0;
+    private int col = 0;
+    private int count = 0;//原数组中不同数值
+
+    public static void main(String[] args) {
+        SparseArray sparseArray = new SparseArray();
+        sparseArray.initArray();
+        sparseArray.showArray();
+        sparseArray.initSparseArray();
+    }
+
+    /**
+     * 创建一个11*11的数组并设置初始值
+     */
+    public void initArray() {
+        row = 11;
+        col = 11;
+        array = new int[row][col];
+        array[1][2] = 1;
+        array[2][3] = 2;
+    }
+
+    /**
+     * 遍历数组
+     */
+    public void showArray() {
+        for (int[] array : array) {
+            for (int item : array) {
+                System.out.printf("%4d", item);
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 初始化稀疏数组
+     */
+    public void initSparseArray() {
+        //查询不同数
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] != 0) {
+                    count++;
+                }
+            }
+        }
+        //创建数组
+        sparseArray = new int[count + 1][3];
+        sparseArray[0][0] = row;
+        sparseArray[0][1] = col;
+        sparseArray[0][2] = count;
+
+        int temp = 0;//计数遍历稀疏数组层次
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] != 0) {
+                    temp++;
+                    sparseArray[temp][0] = i;
+                    sparseArray[temp][1] = j;
+                    sparseArray[temp][2] = array[i][j];
+                }
+            }
+        }
+
+        //遍历稀疏数组
+        System.out.printf("%-5s%-5s%-5s", "row", "col", "val");
+        System.out.println();
+        for (int[] array : sparseArray) {
+            for (int item : array) {
+                System.out.printf("%-5d", item);
+            }
+            System.out.println();
+        }
+    }
+
+}
